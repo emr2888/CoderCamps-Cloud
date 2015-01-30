9de3a90aa48a6c5f54af40b2858f51fed93a1730
emr2888 <emr2888@gmail.com>
2015-01-30 14:42:26 -0600

    Callbacks

---
+++ callbacks/callbacks/JavaScript.js
@@ -0,0 +1,21 @@
+﻿//GET function
+function fbGet(thingToCall) {
+    var request = new XMLHttpRequest();
+    request.open('GET', 'https://cccontact.firebaseio.com/.json', true);
+    request.onload = function () {
+        if (this.status >= 200 && this.status < 400) {
+            //Do Something On Success
+            console.log("Do Something On Success");
+            thingToCall();
+        }
+        else {
+            //Do Something On Error
+        }
+    };
+    request.send();
+}
+fbGet(peanutButter); // call that function fbGet
+
+function peanutButter() {
+    console.log("jelly time");
+};
