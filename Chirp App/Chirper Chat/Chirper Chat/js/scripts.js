5a231f717030417ac64f00938023321ebb9b3c11
emr2888 <emr2888@gmail.com>
2015-01-30 21:35:37 -0600

    Chirp Chat

---
+++ Chirp App/Chirper Chat/Chirper Chat/js/scripts.js
@@ -0,0 +1,40 @@
+// Master Array
+var people = [];
+
+// Chirps Array
+var chirps = [];
+// Person Constructor
+function Person(name, handle, city, url) {
+    this.name = name;
+    this.handle = handle;
+    this.city = city;
+    this.url = url;
+    this.chirps = [];
+};
+
+// Chirps Constructor
+function Chirps(chirp, timeStamp, handle) {
+    this.chirp = chirp;
+    this.timeStamp = timeStamp;
+    this.handle = handle;
+};
+
+/* ID Names - Inputs
+ * Post yor chirp = chirps-input
+ * Post Button = post-button
+ * Friends Area = friend-render
+ * Friends Delete = 
+ * Button My Chirps = myChirps-button
+ * Chirp Display DIV = chirp-display
+ * Edit Pencil =
+ */
+
+//Function Edit Profile
+function editProfile() {
+
+};
+
+//Function Post Your Chirps
+function postChirps() {
+
+};
